6b7d16ab6f9074842a94b7d2d224396d9e787766
Fernando Martino <fernando.martino@sisnet360.com>
2021-04-07 11:36:37 +0200

    Comentarios

--- Proy_JuegoRol/src/juego/mapas/Mapa.java
+++ Proy_JuegoRol/src/juego/mapas/Mapa.java
@@ -49,21 +49,39 @@ public class Mapa {
 
 	public ArrayList<Rectangle> areasColision = new ArrayList<>();
 
+	/**
+	 * Contructor de la clase Mapa.
+	 * 
+	 * @param ruta
+	 */
 	public Mapa(final String ruta) {
 		this.ruta = ruta;
 		String mapa = CargadorRecursos.leerFicheroTexto(ruta);
 		leerMapa(mapa);
 	}
 
+	/**
+	 * Método encargado de actualizar el estado del mapa en cada actualización.
+	 * 
+	 * @param posX
+	 * @param posY
+	 */
 	public void actualizar(final int posX, final int posY) {
 		actualizarAreasColision(posX, posY);
 		actualizarZonaSalida(posX, posY);
 		if (conversaciones != null && !conversaciones.isEmpty())
 			actualizarZonaConversacion(posX, posY);
-		if (combates != null &&!combates.isEmpty())
+		if (combates != null && !combates.isEmpty())
 			actualizarZonaCombates(posX, posY);
 	}
 
+	/**
+	 * Método encargado de dibujar el mapa en pantalla.
+	 * 
+	 * @param g
+	 * @param posX
+	 * @param posY
+	 */
 	public void dibujar(Graphics g, int posX, int posY) {
 		int ladoSprite = Constantes.LADO_SPRITE;
 		for (int i = 0; i < alto; ++i) {
@@ -79,6 +97,12 @@ public class Mapa {
 		}
 	}
 
+	/**
+	 * Método encargado de descomponer el texto recibido del mapa y mandar cada
+	 * parte a analizar.
+	 * 
+	 * @param textoMapa
+	 */
 	private void leerMapa(final String textoMapa) {
 		String[] partes = textoMapa.split("@");
 		ancho = Integer.parseInt(partes[0]);
@@ -107,9 +131,14 @@ public class Mapa {
 
 		String zonaCombates = partes[9];
 		guardarZonaCombates(zonaCombates);
-
 	}
 
+	/**
+	 * Recibe los códigos de las hojas de sprites que necesita el mapa y los
+	 * almacena.
+	 * 
+	 * @param textoHojas
+	 */
 	private void guardarHojas(String[] textoHojas) {
 		hojas = new HojaSprites[textoHojas.length];
 		numeroHojasSprites = new int[textoHojas.length];
@@ -119,6 +148,12 @@ public class Mapa {
 		}
 	}
 
+	/**
+	 * Método encargado de seleccionar y guardar los sprites que se van a
+	 * utilizar en el mapa.
+	 * 
+	 * @param textoSprites
+	 */
 	private void guardarSprites(String[] textoSprites) {
 		sprites = new Sprite[textoSprites.length];
 		numeroSprite = new int[sprites.length];
@@ -132,12 +167,16 @@ public class Mapa {
 				if (hojaSprite == numeroHojasSprites[j])
 					puntero = j;
 			}
-			sprites[i] = hojas[puntero].getSprite(Integer.parseInt(separacionTextoSprite[3]),
-					Integer.parseInt(separacionTextoSprite[2]));
+			sprites[i] = hojas[puntero].getSprite(Integer.parseInt(separacionTextoSprite[3]), Integer.parseInt(separacionTextoSprite[2]));
 		}
-
 	}
 
+	/**
+	 * Método encargado de guardar las zonas a las que hay colisiones en el
+	 * mapa.
+	 * 
+	 * @param textoColisiones
+	 */
 	private void guardarColisiones(String textoColisiones) {
 		mapaColisiones = new boolean[textoColisiones.length()];
 		for (int i = 0; i < textoColisiones.length(); ++i) {
@@ -148,6 +187,12 @@ public class Mapa {
 		}
 	}
 
+	/**
+	 * Método que guarda el orden en que los sprites van a ser dibujados en la
+	 * pantalla.
+	 * 
+	 * @param mapaCuadros
+	 */
 	private void guardarMapaSprites(String mapaCuadros) {
 		ArrayList<Integer> mapaAux = new ArrayList<>();
 		String[] lineas = mapaCuadros.split("#");
@@ -163,6 +208,11 @@ public class Mapa {
 		}
 	}
 
+	/**
+	 * Método que guarda las zonas del mapa en que hay conversaciones.
+	 * 
+	 * @param zona
+	 */
 	private void guardarZonaConversaciones(String zona) {
 		if (!zona.equals("no")) {
 			conversaciones = new ArrayList<Point>();
@@ -180,6 +230,11 @@ public class Mapa {
 		}
 	}
 
+	/**
+	 * Método que guarda las zonas del mapa en que hay combates.
+	 * 
+	 * @param zona
+	 */
 	private void guardarZonaCombates(String zona) {
 		if (!zona.equals("no")) {
 			combates = new ArrayList<>();
@@ -197,6 +252,12 @@ public class Mapa {
 		}
 	}
 
+	/**
+	 * Método que guarda la posición del mapa en que aparece el jugador al
+	 * generarse.
+	 * 
+	 * @param posInicial
+	 */
 	private void guardarPosicionInicial(String posInicial) {
 		String[] coordenadas = posInicial.split("-");
 		posicionInicial = new Point();
@@ -204,6 +265,12 @@ public class Mapa {
 		posicionInicial.y = Integer.parseInt(coordenadas[1]) * Constantes.LADO_SPRITE;
 	}
 
+	/**
+	 * Método para guardar la zona del mapa para avanzar al siguiente mapa y la
+	 * referencia al siguiente mapa.
+	 * 
+	 * @param posSalida
+	 */
 	private void guardarSalida(String posSalida) {
 		String[] coordenadas = posSalida.split("-");
 		posicionSalida = new Point();
@@ -212,6 +279,13 @@ public class Mapa {
 		siguienteMapa = coordenadas[2];
 	}
 
+	/**
+	 * Método encargado de actualizar las zonas de colisión del mapa, en cada
+	 * actualización, según la posición del jugador.
+	 * 
+	 * @param posX
+	 * @param posY
+	 */
 	private void actualizarAreasColision(final int posX, final int posY) {
 		if (!areasColision.isEmpty())
 			areasColision.clear();
@@ -230,6 +304,13 @@ public class Mapa {
 
 	}
 
+	/**
+	 * Método encargado de actualizar la zona de salida del mapa, en cada
+	 * actualización, según la posición del jugador.
+	 * 
+	 * @param posX
+	 * @param posY
+	 */
 	private void actualizarZonaSalida(final int posX, final int posY) {
 		int puntoX = posicionSalida.x * Constantes.LADO_SPRITE - posX + MARGEN_X;
 		int puntoY = posicionSalida.y * Constantes.LADO_SPRITE - posY + MARGEN_Y;
@@ -237,6 +318,13 @@ public class Mapa {
 		zonaSalida = new Rectangle(puntoX, puntoY, Constantes.LADO_SPRITE, Constantes.LADO_SPRITE);
 	}
 
+	/**
+	 * Método encargado de actualizar las zonas de conversación del mapa, en
+	 * cada actualización, según la posición del jugador.
+	 * 
+	 * @param posX
+	 * @param posY
+	 */
 	private void actualizarZonaConversacion(final int posX, final int posY) {
 		if (zonaConversaciones.size() > 0)
 			zonaConversaciones.removeAll(zonaConversaciones);
@@ -249,11 +337,17 @@ public class Mapa {
 		}
 	}
 
+	/**
+	 * Método encargado de actualizar las zonas de combate del mapa, en cada
+	 * actualización, según la posición del jugador.
+	 * 
+	 * @param posX
+	 * @param posY
+	 */
 	private void actualizarZonaCombates(final int posX, final int posY) {
 		if (zonaCombates.size() > 0)
 			zonaCombates.removeAll(zonaCombates);
 		for (int i = 0; i < combates.size(); ++i) {
-			
 
 			int puntoX = combates.get(i).x * Constantes.LADO_SPRITE - posX + MARGEN_X;
 			int puntoY = combates.get(i).y * Constantes.LADO_SPRITE - posY + MARGEN_Y;
@@ -262,6 +356,16 @@ public class Mapa {
 		}
 	}
 
+	/**
+	 * Método que devuelve los bordes de las zonas del mapa para comprobar
+	 * colisiones.
+	 * 
+	 * @param posX
+	 * @param posY
+	 * @param anchoJugador
+	 * @param altoJugador
+	 * @return
+	 */
 	public Rectangle getBordes(final int posX, final int posY, final int anchoJugador, final int altoJugador) {
 		int x = MARGEN_X - posX + anchoJugador;
 		int y = MARGEN_Y - posY + altoJugador;
@@ -270,50 +374,99 @@ public class Mapa {
 		return new Rectangle(x, y, ancho, alto);
 	}
 
+	/**
+	 * Método que devuelve la posición inicial del jugador.
+	 * 
+	 * @return posicionInicial Point
+	 */
 	public Point getPosicionInicial() {
 		return posicionInicial;
 	}
 
-	public Point getPosicionSalida() {
-		return posicionSalida;
-	}
-
+	/**
+	 * Método que devuelve la referencia al siguiente mapa.
+	 * 
+	 * @return siguienteMapa String
+	 */
 	public String getSiguienteMapa() {
 		return siguienteMapa;
 	}
 
+	/**
+	 * Método que devuelve el rectangulo de la salida del mapa.
+	 * 
+	 * @return zonaSalida Rectangle
+	 */
 	public Rectangle getZonaSalida() {
 		return zonaSalida;
 	}
 
+	/**
+	 * Método que devuelve la ruta del mapa actual.
+	 * 
+	 * @return ruta String
+	 */
 	public String getRuta() {
 		return ruta;
 	}
 
+	/**
+	 * Método que quita la conversación y la zona de conversación de la lista de
+	 * conversacaciones.
+	 * 
+	 * @param i
+	 */
 	public void quitarConversacion(int i) {
 		conversaciones.remove(i);
 		zonaConversaciones.remove(i);
 		punteroConversacion.remove(i);
 	}
 
+	/**
+	 * Método que quita el combate y la zona de combate de la lista de combates.
+	 * 
+	 * @param i
+	 */
 	public void quitarCombate(int i) {
 		combates.remove(i);
 		zonaCombates.remove(i);
 		punteroCombates.remove(i);
 	}
 
+	/**
+	 * Método que devuelve la lista con las zonas de conversaciones.
+	 * 
+	 * @return zonaConversaciones ArrayList{Rectangle}
+	 */
 	public ArrayList<Rectangle> getZonaConversaciones() {
 		return zonaConversaciones;
 	}
 
+	/**
+	 * Método que devuelve la lista con las zonas de combates.
+	 * 
+	 * @return zonaCombates ArrayList{Rectangle}
+	 */
 	public ArrayList<Rectangle> getZonaCombates() {
 		return zonaCombates;
 	}
-	
+
+	/**
+	 * Método que devuelve la lista con los punteros de referencias para seleccionar el
+	 * combate.
+	 * 
+	 * @return punteroCombates ArrayList{Integer}
+	 */
 	public ArrayList<Integer> getPunteroCombates() {
 		return punteroCombates;
 	}
-	
+
+	/**
+	 * Método que devuelve la lista con los punteros de referencias para seleccionar la
+	 * cpnversación.
+	 * 
+	 * @return punteroConversaciones ArrayList{Integer}
+	 */
 	public ArrayList<Integer> getPunteroConversacion() {
 		return punteroConversacion;
 	}
