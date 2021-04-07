82ec8e539068cf218cf87774128f15eba639d452
Fernando Martino <fernando.martino@sisnet360.com>
2021-04-07 09:35:40 +0200

    Comentarios

--- Proy_JuegoRol/src/juego/entes/Estadisticas.java
+++ Proy_JuegoRol/src/juego/entes/Estadisticas.java
@@ -55,7 +55,8 @@ public class Estadisticas implements Serializable {
 	/**
 	 * Método que aumenta la salud del personaje con la cantidad indicada.
 	 * 
-	 * @param cura int - Salud a curar.
+	 * @param cura
+	 *            int - Salud a curar.
 	 */
 	public void curar(int cura) {
 		salud += cura;
@@ -70,6 +71,14 @@ public class Estadisticas implements Serializable {
 		salud = saludMaxima;
 	}
 
+	/**
+	 * Método encargado de recibir el ataque físico realizado por el enemigo,
+	 * cambiar la salud y devolver el daño que recibe el personaje.
+	 * 
+	 * @param n
+	 *            int - Ataque físico del enemigo.
+	 * @return ataque int - Daño recibido por el personaje.
+	 */
 	public int recibirAtaqueFisico(int n) {
 		int ataque = 0;
 		if (n > defensa) {
@@ -81,6 +90,14 @@ public class Estadisticas implements Serializable {
 		return ataque;
 	}
 
+	/**
+	 * Método encargado de recibir el ataque mágico realizado por el enemigo,
+	 * cambiar la salud y devolver el daño que recibe el personaje.
+	 * 
+	 * @param n
+	 *            int - Ataque mágico del enemigo.
+	 * @return ataque int - Daño recibido por el personaje.
+	 */
 	public int recibirAtaqueMagico(int n) {
 		int ataque = 0;
 		if (n > defensa) {
@@ -92,62 +109,133 @@ public class Estadisticas implements Serializable {
 		return ataque;
 	}
 
+	/**
+	 * Método que devuelve el ataque físico realizado por el personaje.
+	 * 
+	 * @return ataque int - Ataque físico del personaje.
+	 */
 	public int realizarAtaqueFisico() {
 		Random aleatorio = new Random();
 		int ataqueRealizado = aleatorio.nextInt(ataque);
 		return ataqueRealizado;
 	}
 
+	/**
+	 * Método que devuelve el ataque mágico realizado por el personaje.
+	 * 
+	 * @return ataque int - Ataque mágico del personaje.
+	 */
 	public int realizarAtaqueMagico() {
 		Random aleatorio = new Random();
 		int ataqueRealizado = aleatorio.nextInt(poderMagico);
 		return ataqueRealizado;
 	}
 
+	/**
+	 * Método que devuelve el maná máximo del personaje.
+	 * 
+	 * @return manaMaximo int - Maná máximo del personaje.
+	 */
 	public int getManaMaximo() {
 		return manaMaximo;
 	}
 
+	/**
+	 * Método que devuelve el maná actual del personaje.
+	 * 
+	 * @return mana int - Maná actual del personaje.
+	 */
 	public int getMana() {
 		return mana;
 	}
 
+	/**
+	 * Método que disminuye la cantidad de maná actual.
+	 * 
+	 * @param gasto
+	 *            int - Maná que se gasta.
+	 */
 	public void gastarMana(int gasto) {
 		this.mana -= gasto;
 	}
 
+	/**
+	 * Método que aumenta el maná hasta el máximo.
+	 */
 	public void recuperarTodoMana() {
 		this.mana = this.manaMaximo;
 	}
 
+	/**
+	 * Método que devuelve el ataque físico del personaje.
+	 * 
+	 * @return ataque int - Poder físco del personaje
+	 */
 	public int getAtaque() {
 		return ataque;
 	}
 
+	/**
+	 * Método que devuelve la defensa del personaje.
+	 * 
+	 * @return defensa int - Defensa del personaje
+	 */
 	public int getDefensa() {
 		return defensa;
 	}
 
+	/**
+	 * Método que devuelve el poder mágico del personaje.
+	 * 
+	 * @return poderMagico int - Poder mágico del personaje
+	 */
 	public int getPoderMagico() {
 		return poderMagico;
 	}
 
+	/**
+	 * Método que devuelve la resistencia mágica del personaje.
+	 * 
+	 * @return defensaMagica int - Resistencia mágica del personaje
+	 */
 	public int getDefensaMagica() {
 		return defensaMagica;
 	}
 
+	/**
+	 * Métod que devuelve el nivel del personaje;
+	 * 
+	 * @return
+	 */
 	public int getNivel() {
 		return nivel;
 	}
 
+	/**
+	 * Método que devuelve la experiencia máxima.
+	 * 
+	 * @return expMaxima int - Experiencia máxima
+	 */
 	public int getExpMaxima() {
 		return expMaxima;
 	}
 
+	/**
+	 * Método que devuelve la experiencia actual.
+	 * 
+	 * @return exp int - Experiencia actual
+	 */
 	public int getExp() {
 		return exp;
 	}
 
+	/**
+	 * Método enacargado de aumentar la experiencia y si alcanza la experiencia
+	 * máxima, manda subir de nivel.
+	 * 
+	 * @param exp
+	 *            int - Cantidad de experiencia que aumenta.
+	 */
 	public void aumentarExp(int exp) {
 		this.exp += exp;
 		if (this.exp > expMaxima) {
@@ -155,19 +243,35 @@ public class Estadisticas implements Serializable {
 		}
 	}
 
+	/**
+	 * Método encargado de aumentar el nivel y las estadísticas.
+	 */
 	private void subirNivel() {
 		++nivel;
 		exp -= expMaxima;
+		saludMaxima += 10;
+		manaMaximo += 10;
 		ataque += 5;
 		defensa += 5;
 		poderMagico += 5;
 		defensaMagica += 5;
 	}
 
+	/**
+	 * Método que devuelve la referencia al mapa actual.
+	 * 
+	 * @return mapa int - Referencia al mapa actual.
+	 */
 	public int getMapa() {
 		return mapa;
 	}
 
+	/**
+	 * Método que cambia la referencia al mapa actual.
+	 * 
+	 * @param n
+	 *            int - Referencia del nuevo mapa.
+	 */
 	public void cambiarMapa(int n) {
 		this.mapa = n;
 	}
