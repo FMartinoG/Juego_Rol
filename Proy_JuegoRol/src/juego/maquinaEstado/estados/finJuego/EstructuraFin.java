2b455a13a536962ec1bde523f60125822627a525
Fernando Martino <fernando.martino@sisnet360.com>
2021-04-29 10:54:30 +0200

    Mensajes de final de juego

--- Proy_JuegoRol/src/juego/maquinaEstado/estados/finJuego/EstructuraFin.java
+++ Proy_JuegoRol/src/juego/maquinaEstado/estados/finJuego/EstructuraFin.java
@@ -19,7 +19,8 @@ public class EstructuraFin {
 
 	private boolean finalizado;
 	private String[] textos;
-	private Estadisticas estadisticas;
+	private int resultadoDelJuego;
+
 
 	/**
 	 * Contructor de la estructura.
@@ -27,26 +28,66 @@ public class EstructuraFin {
 	public EstructuraFin(Estadisticas estadisticas) {
 		finalizado = false;
 		inicializarTextos();
-		this.estadisticas = estadisticas;
+		if (estadisticas.getNivel() > 30) {
+			resultadoDelJuego = 3;
+		} else if (estadisticas.getNivel() > 20) {
+			resultadoDelJuego = 2;
+		} else if (estadisticas.getNivel() > 10) {
+			resultadoDelJuego = 1;
+		} else {
+			resultadoDelJuego = 0;
+		}
 	}
 
 	/**
 	 * Método encargado de crear y guardar los mensajes que se van a mostrar.
 	 */
 	private void inicializarTextos() {
-		textos = new String[9];
-		textos[0] = "HACE MUCHO TIEMPO, EN UNA TIERRA AHORA DESCONOCIDA,";
-		textos[1] = "EN LA QUE REINABA LA PAZ Y LA TRANQUILIDAD,";
-		textos[2] = "EXISTÍAN 5 HÉROES QUE VELABAN POR LA SEGURIDAD DEL REINO.";
+		textos = new String[10];
+		
+		if (resultadoDelJuego == 3) {
+			textos[0] = "TRAS LOS ACONTECIMIENTOS SUCEDIDOS";
+			textos[1] = "TODO EL REINO CAMBIÓ PARA SIEMPRE";
+			textos[2] = "...";
+			
+			textos[3] = "TRAS LA MUERTE DEL REY Y DE LOS PRINCIPALES DIRIGENTES";
+			textos[4] = "EL REINO ENTERO ENTRÓ EN PÁNICO";
+			textos[5] = "HASTA QUE UN GRUPO DE NOBLES Y ERUDITOS DECIDIÓ TOMAR EL CONTROL";
+			textos[6] = "Y FORMARON UNA PROSPERA REPÚBLICA.";
 
-		textos[3] = "HASTA QUE UN DÍA, 4 DE ELLOS DESAPARECIERON.";
-		textos[4] = "EL HÉROE QUE QUEDABA APROVECHÓ LA DEBILIDAD DEL REY PARA GANAR PODER.";
-		textos[5] = "TODO ESTO PROVOCÓ UNA GRAN INESTABILIDAD EN TODO EL REINO.";
+			textos[7] = "NUESTRO PROTAGONISTA FUE ENCARCELADO POR LOS ASESINATOS DEL REY Y OTROS NOBLES,";
+			textos[8] = "FINALMENTE FUE EJECUTADO.";
+		} else if (resultadoDelJuego == 2) {
+			textos[0] = "TRAS LOS ACONTECIMIENTOS SUCEDIDOS";
+			textos[1] = "TODO EL REINO CAMBIÓ PARA SIEMPRE";
+			textos[2] = "...";
+			
+			textos[3] = "TRAS LA MUERTE DEL REY Y DE LOS PRINCIPALES DIRIGENTES";
+			textos[4] = "EL REINO ENTERO ENTRÓ EN PÁNICO";
+			textos[5] = "HASTA QUE UN GRUPO DE NOBLES Y ERUDITOS DECIDIÓ TOMAR EL CONTROL";
+			textos[6] = "Y FORMARON UNA PROSPERA REPÚBLICA.";
 
-		textos[6] = "5 AÑOS DESPUÉS...";
+			textos[7] = "EL SOLDADO MISTERIOSO MATÓ AL CONDE, Y FUE ENCARCELADO POR SU MATANZA,";
+			textos[8] = "NUESTRO PROTAGONISTAFUE ENCARCELADO POR LOS ASESINATOS DEL REY,";
+			textos[9] = "FINALMENTE AMBOS FUERON EJECUTADOS.";
+		} else if (resultadoDelJuego == 1) {
+			textos[0] = "DESPUÉS DE SU AVENTURA, NUESTRO PROTAGONISTA VOLVIÓ A CASA.";
+			textos[1] = "CUANDO EL REY SE ENTERÓ DE QUE EL PROTAGONISTA MATÓ AL CONDE";
+			textos[2] = "DECIDIÓ NOMBRARLO GOBERNADOR DE SU PUEBLO.";
+			textos[3] = "(PARECE QUE AL REY NO LE GUSTABA EL CONDE...)";
 
-		textos[7] = "NOS ENCONTRAMOS EN UN PEQUEÑO PUEBLO";
-		textos[8] = "DONDE VIVE UN JOVEN CON SU ABUELO.";
+			textos[4] = "NADIE VOLVIÓ A VER AL SOLDADO MISTERIOSO,";
+			textos[5] = "Y NUESTRO PROTAGONISTA VIVIÓ TRANQUILAMENTE EL RESTO DE SU VIDA";
+		} else {
+			textos[0] = "DESPUÉS DE SU AVENTURA, NUESTRO PROTAGONISTA VOLVIÓ A CASA.";
+			textos[1] = "AUNQUE CUANDO VOLVIÓ SEGUÍA SIN TENER DINERO,";
+			textos[2] = "NADIE SE DIO CUENTA QUE YA NO HABÍA GOBERNADOR,";
+			textos[3] = "ASÍ QUE NO TUVIERON QUE VOLVER A PAGAR IMPUESTOS.";
+
+			textos[4] = "NADIE VOLVIÓ A VER AL SOLDADO MISTERIOSO,";
+			textos[5] = "Y SE ENCONTRÓ EL CADÁVER DEL CONDE SIN NIGUNA HERIDA,";
+			textos[6] = "NUNCA NADIE SUPO COMO MURIÓ.";
+		}
 	}
 
 	/**
@@ -78,25 +119,89 @@ public class EstructuraFin {
 	 * @param g
 	 */
 	private void mostrarHistoria(Graphics g) {
+		if (resultadoDelJuego == 3) {
+			mostrarHistoriaFinal1(g);
+		} else if (resultadoDelJuego == 2) {
+			mostrarHistoriaFinal2(g);
+		} else if (resultadoDelJuego == 1) {
+			mostrarHistoriaFinal3(g);
+		} else {
+			mostrarHistoriaFinal4(g);
+		}
+
+
+	}
+	
+	/**
+	 * Método encargado de mostrar la historia si matas al rey y al traidor.
+	 * 
+	 * @param g
+	 */
+	private void mostrarHistoriaFinal1(Graphics g) {
 		mostrarMensajes(g, 0, 3);
 
 		esperar(2000);
 		ponerPantallaNegro(g);
 
-		mostrarMensajes(g, 3, 3);
-
-		esperar(2000);
-		ponerPantallaNegro(g);
-
-		mostrarMensajes(g, 6, 1);
+		mostrarMensajes(g, 3, 4);
 
 		esperar(2000);
 		ponerPantallaNegro(g);
 
 		mostrarMensajes(g, 7, 2);
+
+		esperar(2000);
+		ponerPantallaNegro(g);
+	}
+	
+	/**
+	 * Método encargado de mostrar la historia si matas al rey y no al traidor.
+	 * 
+	 * @param g
+	 */
+	private void mostrarHistoriaFinal2(Graphics g) {
+		mostrarMensajes(g, 0, 3);
+
 		esperar(2000);
 		ponerPantallaNegro(g);
 
+		mostrarMensajes(g, 3, 4);
+
+		esperar(2000);
+		ponerPantallaNegro(g);
+
+		mostrarMensajes(g, 7, 3);
+
+		esperar(2000);
+		ponerPantallaNegro(g);
+	}
+	
+	/**
+	 * Método encargado de mostrar la historia si matas al traidor y no al rey.
+	 * 
+	 * @param g
+	 */
+	private void mostrarHistoriaFinal3(Graphics g) {
+		mostrarMensajes(g, 0, 4);
+
+		esperar(2000);
+		ponerPantallaNegro(g);
+
+		mostrarMensajes(g, 4, 2);
+	}
+	
+	/**
+	 * Método encargado de mostrar la historia si no matas al rey ni al traidor.
+	 * 
+	 * @param g
+	 */
+	private void mostrarHistoriaFinal4(Graphics g) {
+		mostrarMensajes(g, 0, 4);
+
+		esperar(2000);
+		ponerPantallaNegro(g);
+
+		mostrarMensajes(g, 4, 3);
 	}
 
 	/**
